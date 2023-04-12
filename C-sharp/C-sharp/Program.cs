20e176efe0f87516fa9aea71b35a2bc3134bd5d6
s0-p <b00v0429@gmail.com>
2023-04-12 20:05:50 +0900

    value type

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -10,6 +10,12 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("int형의 크기: " + sizeof(int) + "최소 값: " + int.MinValue + "최대 값: " + int.MaxValue);
+            Console.WriteLine("long형의 크기: " + sizeof(long) + "최소 값: " + long.MinValue + "최대 값: " + long.MaxValue);
+            Console.WriteLine("char형의 크기: " + sizeof(char) + "최소 값: " + char.MinValue + "최대 값: " + char.MaxValue);
+            Console.WriteLine("float형의 크기: " + sizeof(float) + "최소 값: " + float.MinValue + "최대 값: " + float.MaxValue);
+            Console.WriteLine("double형의 크기: " + sizeof(double) + "최소 값: " + double.MinValue + "최대 값: " + double.MaxValue);
+            Console.WriteLine("decimal형의 크기: " + sizeof(decimal) + "최소 값: " + decimal.MinValue + "최대 값: " + decimal.MaxValue);
         }
     }
 }
