390873611f34c00704dc72e29e248d3bd1cf5013
s0-p <b00v0429@gmail.com>
2023-04-13 18:36:06 +0900

    class

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+struct Monster
+{
+    public int hp;
+    public int mp;
+}
+class Hero
+{
+    public int hp;
+    public int mp;
+}
 namespace C_sharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 10;
-            int num2 = 20;
-            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);  //변수 대입 서식 변경
+            Monster monster;    //value type
+            monster.hp = 100;
+            monster.mp = 200;
+
+            Hero hero = new Hero(); // 생성자로 Hero 크기만큼 heap 메모리 할당 -> reference type
+            // hero <- 객체, instance화하다(instansiate)
+            hero.hp = 400;
+            hero.mp = 100;
         }
     }
 }
