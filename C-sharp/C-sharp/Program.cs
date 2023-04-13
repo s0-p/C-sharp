45ddb9684211ad1b819b0a7a93d042b67de62374
s0-p <b00v0429@gmail.com>
2023-04-13 18:12:17 +0900

    variable assignment format

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -10,9 +10,9 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            char ch = 'B';
-            Console.WriteLine("char형 크기: " + sizeof(char) + "byte");
-            Console.WriteLine("char 변수 ch의 값: " + ch);
+            int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);  //변수 대입 서식 변경
         }
     }
 }
