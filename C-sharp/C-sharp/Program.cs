54413b9f482fcb003ad1ee9f4d0f08a519549214
s0-p <b00v0429@gmail.com>
2023-04-14 18:52:31 +0900

    Console.ReadLine(), int.Parse()

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -10,19 +10,12 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            string str = "    world";
-            string str2 = "HELLO";
-            Console.WriteLine(str.Insert(1, "HELLO"));
-            Console.WriteLine(str.PadLeft(10));
-            Console.WriteLine(str.PadLeft(10, '*'));
-            Console.WriteLine(str.PadRight(10));
-            Console.WriteLine(str.PadRight(10, '*'));
-            Console.WriteLine(str.Remove(4));
-            Console.WriteLine(str.Remove(4, 2));
-            Console.WriteLine(str.ToUpper());
-            Console.WriteLine(str2.ToLower());
-            Console.WriteLine(str.Trim());
-            // str을 변경하는 것이 아닌 새로운 문자열을 만들어 출력
+            int num;
+            string strnum = "456";
+            num = int.Parse(strnum);
+            Console.WriteLine("변환한 숫자: {0}", num);
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("입력한 숫자: {0}", num);
         }
     }
 }
