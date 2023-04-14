7f66a99dd43d9a9615779891ad84dfa5d7ea640c
s0-p <b00v0429@gmail.com>
2023-04-14 18:10:38 +0900

    string.Foramt

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -10,13 +10,8 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            char[] chArr = new char[] { 'h', 'e', 'l', 'l', 'o' };
-            string str1 = new string(chArr);
-            string str2 = "hello";
-            string str3 = "world";
-            Console.WriteLine(str1 == str2);    //True <- 주소를 갖고있지만 값 비교로 간주
-            Console.WriteLine(string.ReferenceEquals(str1, str2));  //False
-            Console.WriteLine(str1.CompareTo(str3));    //-1 <- str1 > str3
+            string str = string.Format("이름: {0} 나이: {1}", "박소영", 24);
+            Console.WriteLine(str);
         }
     }
 }
