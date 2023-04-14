562745e372533a11a05e6cec175869529d656f0b
s0-p <b00v0429@gmail.com>
2023-04-14 19:26:35 +0900

    namespace example

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -4,30 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace A
+{
+    class Point
+    {
+        private int x, y;
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
+namespace B
+{
+    class Point
+    {
+        private int x, y;
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
+
 namespace C_sharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            bool isTry = false;
-            do
-            {
-                isTry = false;
-                try
-                {
-                    num = int.Parse(Console.ReadLine());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    isTry = true;
-                }
-            } while (isTry);
-
-            
-            Console.WriteLine(num);
+            A.Point point1 = new A.Point(2, 3);
+            B.Point point2 = new B.Point(4, 5);
         }
     }
 }
