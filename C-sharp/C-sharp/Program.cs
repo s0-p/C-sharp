45236642069d5468e861b2601b1543df305223b2
s0-p <b00v0429@gmail.com>
2023-04-14 18:29:19 +0900

    string method

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -10,8 +10,19 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            string str = string.Format("이름: {0} 나이: {1}", "박소영", 24);
-            Console.WriteLine(str);
+            string str = "    world";
+            string str2 = "HELLO";
+            Console.WriteLine(str.Insert(1, "HELLO"));
+            Console.WriteLine(str.PadLeft(10));
+            Console.WriteLine(str.PadLeft(10, '*'));
+            Console.WriteLine(str.PadRight(10));
+            Console.WriteLine(str.PadRight(10, '*'));
+            Console.WriteLine(str.Remove(4));
+            Console.WriteLine(str.Remove(4, 2));
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine(str2.ToLower());
+            Console.WriteLine(str.Trim());
+            // str을 변경하는 것이 아닌 새로운 문자열을 만들어 출력
         }
     }
 }
