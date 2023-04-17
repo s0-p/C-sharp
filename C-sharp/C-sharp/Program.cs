c85235035b28ea2ece7d932f05661a792ce08b62
s0-p <b00v0429@gmail.com>
2023-04-17 19:13:08 +0900

    out keyword

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -4,34 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Student
-{
-    public string m_name;
-    public static string m_schoolName;
-    public string NAME { get { return m_name; } set { m_name = value; } }
-    public void Intro()
-    {
-        Console.WriteLine("{0}에 다니는 {1}입니다.", m_schoolName, m_name);
-    }
-    public static void IntroMyUniv()
-    {
-        Console.WriteLine("우리학교는 {0}입니다.", m_schoolName);
-    }
-}
 namespace C_sharp
 {
     class Program
     {
+        static void Add(int c, int d, out int sum)
+        {
+            sum = c + d;
+        }
         static void Main(string[] args)
         {
-            Student.m_schoolName = "서울대학교";
-            Student std1 = new Student();
-            Student std2 = new Student();
-            std1.m_name = "홍길동";
-            std2.m_name = "아무개";
-            std1.Intro();
-            std2.Intro();
-            Student.IntroMyUniv();
+            int num1 = 10, num2 = 20;
+            int sum;
+            Add(num1, num2, out sum);
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, sum);
         }
     }
 }
