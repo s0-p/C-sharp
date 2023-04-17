8f84f034700181e963b072f321d7b8a2e2c7dc2d
s0-p <b00v0429@gmail.com>
2023-04-17 19:28:22 +0900

    foreach

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -8,27 +8,14 @@ namespace C_sharp
 {
     class Program
     {
-        static void Swap(ref int num1, ref int num2)
-        {
-            int tmp = num1;
-            num1 = num2;
-            num2 = tmp;
-        }
-        static void Increment(ref int num)
-        {
-            num++;
-        }
         static void Main(string[] args)
         {
-            int num1 = 10;
-            int num2 = 20;
-            Console.WriteLine("현재 값 : {0}", num1);
-            Increment(ref num1);
-            Console.WriteLine("증가 후 값 : {0}", num1);
-
-            Console.WriteLine("현재 num1 값: {0} num2 값: {1}", num1, num2);
-            Swap(ref num1, ref num2);
-            Console.WriteLine("swap 후 num1 값: {0} num2 값: {1}", num1, num2);
+            string str = "Hello World";
+            foreach (char c in str)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
         }
     }
 }
