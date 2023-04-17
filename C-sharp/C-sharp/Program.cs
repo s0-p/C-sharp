7561ff35cec38a82dd3fc1e17314ec34b08a545b
s0-p <b00v0429@gmail.com>
2023-04-17 19:36:41 +0900

    for vs foreach

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -10,12 +10,19 @@ namespace C_sharp
     {
         static void Main(string[] args)
         {
-            string str = "Hello World";
-            foreach (char c in str)
+            int i = 0;
+            int[] arr = new int[5] { 10, 20, 30, 40, 50 };
+
+            foreach (int n in arr)
             {
-                Console.Write(c);
+                Console.WriteLine("arr[{0}] : {1}", i, n);
+                i++;
             }
             Console.WriteLine();
+            for (int n = 0; n < arr.Length; n++)
+            {
+                Console.WriteLine("arr[{0}] : {1}", n, arr[n]);
+            }
         }
     }
 }
