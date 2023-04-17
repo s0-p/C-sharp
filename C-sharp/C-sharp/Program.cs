b1674cc2b682fd1c187b7d49dc24356dfa58dfff
s0-p <b00v0429@gmail.com>
2023-04-17 18:51:26 +0900

    static method

--- C-sharp/C-sharp/Program.cs
+++ C-sharp/C-sharp/Program.cs
@@ -13,6 +13,10 @@ class Student
     {
         Console.WriteLine("{0}에 다니는 {1}입니다.", m_schoolName, m_name);
     }
+    public static void IntroMyUniv()
+    {
+        Console.WriteLine("우리학교는 {0}입니다.", m_schoolName);
+    }
 }
 namespace C_sharp
 {
@@ -24,9 +28,10 @@ namespace C_sharp
             Student std1 = new Student();
             Student std2 = new Student();
             std1.m_name = "홍길동";
-            std1.m_name = "아무개";
+            std2.m_name = "아무개";
             std1.Intro();
             std2.Intro();
+            Student.IntroMyUniv();
         }
     }
 }
