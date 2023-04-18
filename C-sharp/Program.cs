2c913ccfcec416c4209d6efa1c8b560d18b05fc1
s0-p <b00v0429@gmail.com>
2023-04-18 19:56:38 +0900

    indexor applications and error handling

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,40 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class TempRecord
+class DayCollection
 {
-    private float[] temps = new float[10] {56.1F, 56.2F, 56.3F, 56.4F, 56.5F,
-                                    56.6F, 56.7F, 56.8F, 56.9F, 57F};
-    public int Length { get { return temps.Length; } }
-    public float this[int index]
+    private string[] days = { "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" };
+    private int GetDay(string testDay)
     {
-        get 
+        for (int i = 0; i < days.Length; i++)
         {
-            return temps[index];
+            if (days[i] == testDay)
+            {
+                return i;
+            }
         }
-
-        set 
+        throw new ArgumentOutOfRangeException(testDay, "testDay must be in the form \"Sun\", \"Mon\", etc");
+    }
+    public int this[string day]     //indexor
+    {
+        get
         {
-            temps[index] = value;
+            return GetDay(day);
         }
     }
-
-
 }
-
 namespace C_sharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TempRecord tempRecord = new TempRecord();
-            tempRecord[3] = 58.3F;
-            tempRecord[5] = 60.1F;
-            for (int i = 0; i < tempRecord.Length; i++)
-            {
-                Console.WriteLine("tempRecord[{0}] = {1}", i, tempRecord[i]);
-            }
+            DayCollection week = new DayCollection();
+            Console.WriteLine(week["Fri"]);
+
+            //ArgumentOutOfRangeException
+            Console.WriteLine(week["Made-up Day"]);
         }
     }
 }
