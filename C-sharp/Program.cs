2e2849fb996f15080b4d495b7e64508aa85ef6ac
s0-p <b00v0429@gmail.com>
2023-04-19 19:15:12 +0900

    Array.Copy()

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -8,20 +8,17 @@ namespace C_sharp
 {   
     class Program
     {
-        static void PrintArray(int[,] arr)
-        {
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.WriteLine("arr[{0}, {1}] = {2}", i, j, arr[i, j]);
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            PrintArray(new int [,] { { 1, 2 }, { 3, 4 }, { 5,6 }, { 7, 8 } });
+            int[] orgArr = new int[] { -1, -3, -5, -7, -9 };
+            int[] copyArr = { 2, 4, 6, 8, 10 };
+            
+            //orgArr의 0부터 copyArr의 2부터에 3개 복사
+            Array.Copy(orgArr, 0, copyArr, 2, 3);
+            for (int i = 0; i < copyArr.Length; i++)
+            {
+                Console.WriteLine("copyArr[{0}] = {1}", i, copyArr[i]);
+            }
         }
     }
 }
