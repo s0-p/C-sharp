a503940795e0011b87e2f5c4a97b0a93d6bf7688
s0-p <b00v0429@gmail.com>
2023-04-19 18:57:00 +0900

    array parameter

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -6,36 +6,23 @@ using System.Threading.Tasks;
 
 namespace C_sharp
 {   
-    class PocketMon
+   class ArrayClass
     {
-        private string kind;
-        private string name;
-        public PocketMon(string kind, string name)
+        static void PrintArray(string[] arr)
         {
-            this.kind = kind;
-            this.name = name;
-        }
-        public void PrintPocketMon()
-        {
-            Console.WriteLine("종류 : {0} / 이름 : {1}", kind, name);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "{0}", i < arr.Length - 1 ? " " : "");
+            }
+            Console.WriteLine();
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            PocketMon[] pocketMons = new PocketMon[4]; // 배열 생성
-            
-            //각각의 객체 생성 필요
-            pocketMons[0] = new PocketMon("노말", "잠만보");
-            pocketMons[1] = new PocketMon("불꽃", "파이리");
-            pocketMons[2] = new PocketMon("전기", "피카츄");
-            pocketMons[3] = new PocketMon("물", "꼬부기");
-
-            foreach (PocketMon po in pocketMons)
-            {
-                po.PrintPocketMon();
-            }
+            string[] weekDays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            PrintArray(weekDays);
         }
     }
 }
