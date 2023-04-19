d77433266f88747cd93cb5b1820ad02c2e0375e9
s0-p <b00v0429@gmail.com>
2023-04-19 18:25:03 +0900

    multi-demensional array

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,43 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class DayCollection
-{
-    #region Constants and Fields
-    private string[] days = { "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" };
-    #endregion
-    #region Public Properties
-    private int GetDay(string testDay)
-    {
-        for (int i = 0; i < days.Length; i++)
-        {
-            if (days[i] == testDay)
-            {
-                return i;
-            }
-        }
-        throw new ArgumentOutOfRangeException(testDay, "testDay must be in the form \"Sun\", \"Mon\", etc");
-    }
-    public int this[string day]     //indexor
-    {
-        get
-        {
-            return GetDay(day);
-        }
-    }
-    #endregion
-}
 namespace C_sharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DayCollection week = new DayCollection();
-            Console.WriteLine(week["Fri"]);
-
-            //ArgumentOutOfRangeException
-            Console.WriteLine(week["Made-up Day"]);
+            int i = 0;
+            int[,] array = new int[2, 3]
+            {
+                {1, 2, 3},
+                {4, 5, 6}
+            };
+            foreach(int num in array)
+            {
+                Console.WriteLine(num + " ");
+                i++;
+                if (i % array.GetLength(1) == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
