367fc5e4425237e1d740d58c045aaa2f7622a080
s0-p <b00v0429@gmail.com>
2023-04-19 20:08:10 +0900

    generalization example

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -3,6 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+class List<T>
+{
+    private T[] arr;
+    public List()
+    {
+        arr = new T[2];
+    }
+    public int Length { get { return arr.Length; } }
+    public void InitArray(int index, T value)
+    {
+        arr[index] = value;
+    }
+    public T GetValue(int index)
+    {
+        return arr[index];
+    }
+}    
 
 namespace C_sharp
 {   
@@ -14,14 +31,23 @@ namespace C_sharp
         }
         static void Main(string[] args)
         {
-            string name = "박소영";
-            int age = 24;
-            float heignt = 180;
-            double weight = 70.5;
-            Print(name);
-            Print(age);
-            Print(heignt);
-            Print(weight);
+            List<int> list1 = new List<int>();
+            List<float> list2 = new List<float>();
+            List<string> list3 = new List<string>();
+            list1.InitArray(0, 58);
+            list1.InitArray(1, 30);
+            list2.InitArray(0, 75.2f);
+            list2.InitArray(1, 65.5f);
+            list3.InitArray(0, "아무개");
+            list3.InitArray(1, "홍길동");
+            for (int i = 0; i < list1.Length; i++)
+            {
+                Print("이름: " + list3.GetValue(i));
+                Print("나이: " + list1.GetValue(i));
+                Print("몸무게: " + list2.GetValue(i));
+                Print(" ");
+            }
+
         }
     }
 }
