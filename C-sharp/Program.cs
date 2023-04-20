e9eb87f9417b0ae3c55c99d08f79a6caf7b9d5cc
s0-p <b00v0429@gmail.com>
2023-04-20 18:50:16 +0900

    generalization + delegate

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,22 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-delegate int TestDelegate(int a, int b);
+delegate T TestDelegate<T>(T a, T b);
 class Calculator
 {
     public int Plus(int a, int b)
     {
-        Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+        return a + b;
+    }
+    public float Plus(float a, float b)
+    {
         return a + b;
     }
     public int Minus(int a, int b)
     {
-        Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+        return a - b;
+    }
+    public float Minus(float a, float b)
+    {
         return a - b;
     }
     public int Multiply(int a, int b)
     {
-        Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+        return a * b;
+    }
+    public float Multiply(float a, float b)
+    {
         return a * b;
     }
 }
@@ -28,21 +37,18 @@ namespace C_sharp
 {   
     class Program
     {
+        public static void PrintCal<T>(T a, T b, TestDelegate<T> callback)
+        {
+            Console.WriteLine(callback(a, b));
+        }
        
         static void Main(string[] args)
         {
             Calculator cal = new Calculator();
-            TestDelegate del = new TestDelegate(cal.Plus);
-            del += cal.Minus;
-            del += cal.Multiply;
-            Console.WriteLine("del : Plus, Minus, Multiply");
-            del(10, 20);
-
-            Console.WriteLine();
-            del -= cal.Plus;
-            del -= cal.Minus;
-            Console.WriteLine("del : Multiply");
-            del(10, 20);
+            PrintCal<int>(10, 20, cal.Plus);
+            PrintCal<float>(54.6f, 96.32f, cal.Plus);
+            PrintCal<float>(200.5f, 120.10f, cal.Multiply);
+            PrintCal<float>(36.5f, 42.3f, cal.Multiply);
         }
     }
 }
