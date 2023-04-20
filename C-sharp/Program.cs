91fcd6a24b872c0042af7c166adbed7a85e1c0d1
s0-p <b00v0429@gmail.com>
2023-04-20 18:33:40 +0900

    delegate example

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -2,30 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;  
+using System.Threading.Tasks;
+
+delegate int TestDelegate(int a, int b);
+class Calculator
+{
+    public int Plus(int a, int b)
+    {
+        return a + b;
+    }
+    public int Minus(int a, int b)
+    {
+        return a - b;
+    }
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
 
 namespace C_sharp
 {   
     class Program
     {
-        delegate int TestDelegate(int a, int b);
-        public static int Plus(int a, int b)
+        public static void PrintCal(int a, int b, TestDelegate callback)
         {
-            return a + b;
+            Console.WriteLine(callback(a, b));
         }
-        public static int Minus(int a, int b)
-        {
-            return a - b;
-        }
-
         static void Main(string[] args)
         {
-            TestDelegate calculate = new TestDelegate(Plus);
-            int result = calculate(20, 30);
-            Console.WriteLine("20 + 30 = " + result);
-            calculate = new TestDelegate(Minus);
-            result = calculate(30, 20);
-            Console.WriteLine("30 - 20 = " + result);
+            Calculator cal = new Calculator();
+            PrintCal(10, 20, cal.Plus);
+            PrintCal(10, 20, cal.Minus);
+            PrintCal(10, 20, cal.Multiply);
+
         }
     }
 }
