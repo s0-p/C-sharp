d9a451636f6aa7e97726b7a9607b2100607f4094
s0-p <b00v0429@gmail.com>
2023-04-21 19:40:29 +0900

    Queue

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -3,101 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Bookstore
+using System.Collections;
+namespace C_sharp
 {
-    using System.Collections;
-
-    public struct Book
+    class Program
     {
-        public string Title;
-        public string Author;
-        public decimal Price;
-        public bool Paperback;
-
-        public Book(string title, string author, decimal price, bool paperBack)
-        {
-            Title = title;
-            Author = author;
-            Price = price;
-            Paperback = paperBack;
-        }
-    }
-
-    public delegate void ProcessBookDelegate(Book book);
-
-    public class BookDB
-    {
-        ArrayList list = new ArrayList();
-
-        public void AddBook(string title, string author, decimal price, bool paperBack)
-        {
-            list.Add(new Book(title, author, price, paperBack));
-        }
-
-        public void ProcessPaperbackBooks(ProcessBookDelegate processBook)
-        {
-            foreach (Book b in list)
-            {
-                if (b.Paperback)
-                    processBook(b);
-            }
-        }
-    }
-}
-
-
-namespace BookTestClient
-{
-    using Bookstore;
-    class PriceTotaller
-    {
-        int countBooks = 0;
-        decimal priceBooks = 0.0m;
-
-        internal void AddBookToTotal(Book book)
-        {
-            countBooks += 1;
-            priceBooks += book.Price;
-        }
-
-        internal decimal AveragePrice()
-        {
-            return priceBooks / countBooks;
-        }
-    }
-
-    class TestBookDB
-    {
-        static void PrintTitle(Book b)
-        {
-            System.Console.WriteLine("   {0}", b.Title);
-        }
-
         static void Main()
         {
-            BookDB bookDB = new BookDB();
-
-            AddBooks(bookDB);
-
-            System.Console.WriteLine("Paperback Book Titles:");
-
-            bookDB.ProcessPaperbackBooks(PrintTitle);
-
-            PriceTotaller totaller = new PriceTotaller();
-
-            bookDB.ProcessPaperbackBooks(totaller.AddBookToTotal);
-
-            System.Console.WriteLine("Average Paperback Book Price: ${0:#.##}",
-                    totaller.AveragePrice());
-        }
-
-        static void AddBooks(BookDB bookDB)
-        {
-            bookDB.AddBook("The C Programming Language", "Brian W. Kernighan and Dennis M. Ritchie", 19.95m, true);
-            bookDB.AddBook("The Unicode Standard 2.0", "The Unicode Consortium", 39.95m, true);
-            bookDB.AddBook("The MS-DOS Encyclopedia", "Ray Duncan", 129.95m, false);
-            bookDB.AddBook("Dogbert's Clues for the Clueless", "Scott Adams", 12.00m, true);
+            Queue que = new Queue();
+            que.Enqueue(10);
+            que.Enqueue(20);
+            que.Enqueue(30);
+            que.Dequeue();
+            que.Enqueue(4.4);
+            que.Dequeue();
+            que.Enqueue("ABC");
+            while (que.Count > 0)
+            {
+                Console.WriteLine(que.Dequeue());
+            }
         }
     }
 }
