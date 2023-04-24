be16c29a169ff05a53c388f49aa25c2bcddf85ca
s0-p <b00v0429@gmail.com>
2023-04-24 18:23:01 +0900

    hashtable

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
+using System.Collections;   //object형
+
 namespace C_sharp
 {
     class Program
     {
         static void Main()
         {
-            Stack stack = new Stack();
-            stack.Push(10);
-            stack.Push(20);
-            stack.Push(30);
-            stack.Pop();    //  30
-            stack.Push(4.4);
-            stack.Pop();    // 4.4
-            stack.Push("ABC");
-            while (stack.Count > 0)
+            Hashtable ht = new Hashtable();
+            if (!ht.ContainsKey("오렌지"))
             {
-                Console.WriteLine(stack.Pop());     //LIFO
+                ht.Add("오렌지", "Orange");
             }
+            if (!ht.ContainsKey("바나나"))
+            {
+                ht.Add("바나나", "Banana");
+            }
+            if (!ht.ContainsKey("사과"))
+            {
+                ht.Add("사과", "Apple");
+            }
+            foreach (string s in ht.Keys)
+            {
+                Console.WriteLine(s);
+                Console.WriteLine(ht[s]);
+            }
+            Console.WriteLine(ht["오렌지"]);
+            Console.WriteLine(ht["바나나"]);
+            Console.WriteLine(ht["사과"]);
         }
     }
 }
