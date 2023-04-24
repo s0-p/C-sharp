440eb46287b8e4bf11ef0fafdfb26bc9bf989379
s0-p <b00v0429@gmail.com>
2023-04-24 18:59:04 +0900

    sortedList

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -10,15 +10,20 @@ namespace C_sharp
     {
         static void Main()
         {
-            Dictionary<string, string> genDic = new Dictionary<string, string>();
-            genDic.Add("txt", "notepad.exe");
-            genDic.Add("bmp", "paint.exe");
-            genDic.Add("mp3", "wmplayer.exe");
-            Console.WriteLine("[Dictionary]");
-            foreach (KeyValuePair<string, string> kvp in genDic)
+            SortedList<int, string> colorSort = new SortedList<int, string>();
+            colorSort.Add(1, "Red");
+            colorSort.Add(3, "Green");
+            colorSort.Add(2, "Blue");
+            foreach (KeyValuePair<int, string> color in colorSort)
             {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                Console.WriteLine("Key {0} Color {1}", color.Key, color.Value);
             }
+            Console.WriteLine("현재 리스트 크기: {0}", colorSort.Capacity);
+            colorSort.TrimExcess();
+            Console.WriteLine("현재 리스트 크기: {0}, 요소의 개수: {1}", colorSort.Capacity, colorSort.Count);
+            colorSort.Remove(2);
+            colorSort.TrimExcess();
+            Console.WriteLine("현재 리스트 크기: {0}, 요소의 개수: {1}", colorSort.Capacity, colorSort.Count);
         }
     }
 }
