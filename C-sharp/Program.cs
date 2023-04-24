90169e1ec08a1a27c92f66590327796114270415
s0-p <b00v0429@gmail.com>
2023-04-24 19:49:16 +0900

    generic queue

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -10,20 +10,16 @@ namespace C_sharp
     {
         static void Main()
         {
-            List<string> list = new List<string>();
-            list.Add("트럼프");
-            list.Add("오바마");
-            list.Add("힐러리");
-            for (int i = 0; i < list.Count; i++)
+            Queue<int> genQueue = new Queue<int>();
+            genQueue.Enqueue(1);
+            genQueue.Enqueue(2);
+            genQueue.Enqueue(3);
+            Console.WriteLine("[Queue]");
+            while(genQueue.Count > 0)
             {
-                Console.WriteLine("{0}. {1}대통령", i, list[i]);
+                Console.WriteLine("Value = {0}", genQueue.Dequeue());
             }
-            Console.WriteLine("컬렉션의 현재 배열 크기 {0}", list.Capacity);
-            Console.WriteLine("컬렉션에 저장된 요소 개수 {0}", list.Count);
-            list.Insert(1, "부시");
-            Console.WriteLine("리스트 1번째 대통령 {0}", list[1]);
-            list.RemoveAt(1);
-            Console.WriteLine("리스트 1번째 대통령 {0}", list[1]);
+            Console.WriteLine();
         }
     }
 }
