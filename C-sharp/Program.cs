0efd41ff30eb9238e38ccc1046b486a0c6b75f45
s0-p <b00v0429@gmail.com>
2023-04-24 19:28:17 +0900

    LinkedListNode

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -10,17 +10,15 @@ namespace C_sharp
     {
         static void Main()
         {
-            LinkedList<string> genLL = new LinkedList<string>();
-            genLL.AddLast("4등");
-            genLL.AddFirst("1등");
-            genLL.AddAfter(genLL.Find("1등"), "2등");
-            genLL.AddBefore(genLL.Find("4등"), "3등");
-            Console.WriteLine("[LinkedList]");
-            genLL.Remove("1등");
-            foreach (string str in genLL)
-            {
-                Console.WriteLine("Value = {0}", str);
-            }
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(1);
+            LinkedListNode<int> firstNode = linkedList.First;
+            LinkedListNode<int> currentNode = linkedList.AddAfter(firstNode, 3);
+            currentNode = linkedList.AddBefore(currentNode, 2);
+            Console.WriteLine("첫번째 노드의 값: {0}", currentNode.Previous.Value);
+            Console.WriteLine("두번째 노드의 값: {0}", currentNode.Value);
+            Console.WriteLine("세번째 노드의 값: {0}", currentNode.Next.Value);
+
         }
     }
 }
