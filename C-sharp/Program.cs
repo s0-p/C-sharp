86bbeb5447a3e082635e5ed9be59ae6228ef95ba
s0-p <b00v0429@gmail.com>
2023-04-24 18:32:03 +0900

    dictionary

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -3,35 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;   //object형
-
+using System.Collections.Generic;
 namespace C_sharp
 {
     class Program
     {
         static void Main()
         {
-            Hashtable ht = new Hashtable();
-            if (!ht.ContainsKey("오렌지"))
+            Dictionary<string, string> genDic = new Dictionary<string, string>();
+            genDic.Add("txt", "notepad.exe");
+            genDic.Add("bmp", "paint.exe");
+            genDic.Add("mp3", "wmplayer.exe");
+            Console.WriteLine("[Dictionary]");
+            foreach (KeyValuePair<string, string> kvp in genDic)
             {
-                ht.Add("오렌지", "Orange");
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
             }
-            if (!ht.ContainsKey("바나나"))
-            {
-                ht.Add("바나나", "Banana");
-            }
-            if (!ht.ContainsKey("사과"))
-            {
-                ht.Add("사과", "Apple");
-            }
-            foreach (string s in ht.Keys)
-            {
-                Console.WriteLine(s);
-                Console.WriteLine(ht[s]);
-            }
-            Console.WriteLine(ht["오렌지"]);
-            Console.WriteLine(ht["바나나"]);
-            Console.WriteLine(ht["사과"]);
         }
     }
 }
