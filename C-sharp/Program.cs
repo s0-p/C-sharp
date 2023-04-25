fecd165c7470aba34032eda288abb16aadce382d
s0-p <b00v0429@gmail.com>
2023-04-25 18:42:00 +0900

    new

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -6,41 +6,20 @@ using System.Threading.Tasks;
 
 namespace C_sharp
 {
-    class Program
+    class BaseClass
     {
-        class BaseClass
-        {
-            public virtual void Method1()   //가상 함수
-            {
-                Console.WriteLine("부모클래스 - Method1");
-            }
-            public void Method2()
-            {
-                Console.WriteLine("부모클래스 - Method2");
-            }
-        }
-        class DreivedClass : BaseClass
-        {
-            public override void Method1()   //덮어쓰기
-            {
-                Console.WriteLine("자식클래스 - Method1");
-            }
-            public new void Method2()   //한정자 ->  자식클래스 한정으로 새로 정의
-            {
-                Console.WriteLine("자식클래스 - Method2");
-            }
+        public static int x = 55;
+        public static int y = 22;
+    }
+    class DreivedClass : BaseClass
+    {
+        new public static int x = 100;
 
-            static void Main(string[] args)
-            {
-                BaseClass bc = new BaseClass();
-                DreivedClass dc = new DreivedClass();
-                BaseClass bcdc = new DreivedClass();
-                bc.Method1();
-                dc.Method1();
-                dc.Method2();
-                bcdc.Method1();
-                bcdc.Method2();
-            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(x);   //100
+            Console.WriteLine(BaseClass.x); //55
+            Console.WriteLine(y);   //22
         }
     }
 }
