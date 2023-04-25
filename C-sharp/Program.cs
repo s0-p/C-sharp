dc468f1567ff4a5952d564decdd0b8218616d85b
s0-p <b00v0429@gmail.com>
2023-04-25 19:24:48 +0900

    inheritance example

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,29 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class BaseC
+class Car
 {
-    public class NestedC
+    public virtual void DescribeCar()
     {
-        public int x = 200;
-        public int y;
+        Console.WriteLine("4개의 휠이 달린 바퀴와 엔진이 있습니다.");
+        ShowDetails();
+    }
+    public virtual void ShowDetails()
+    {
+        Console.WriteLine("4명의 사람을 태울 수 있습니다.");
+    }
+}
+class ConvertibleCar : Car
+{
+    //DescribeCar()는 부모클래스로 접근하므로 TestCars1에서 적용 X
+    public new void ShowDetails()
+    {
+        Console.WriteLine("지붕이 열립니다.");
+    }
+}
+class Minivan : Car
+{
+    public override void ShowDetails()
+    {
+        Console.WriteLine("7명을 태울 수 있습니다.");
     }
 }
 
-public class DrivedC : BaseC
+class Program
 {
-    new public class NestedC
+    public static void TestCars1()
     {
-        public int x = 100;
-        public int y;
-        public int z;
-    }
+        Console.WriteLine("\nTestCars1");
+        Console.WriteLine("-----------");
 
-    static void Main(string[] args)
+        Car car1 = new Car();
+        car1.DescribeCar();
+        Console.WriteLine("-----------");
+
+        ConvertibleCar car2 = new ConvertibleCar();
+        car2.DescribeCar();
+        Console.WriteLine("-----------");
+
+        Minivan car3 = new Minivan();
+        car3.DescribeCar();
+        Console.WriteLine("-----------");
+    }
+    public static void TestCars2()
     {
-        NestedC c1 = new NestedC();
-        BaseC.NestedC c2 = new BaseC.NestedC();
-        Console.WriteLine(c1.x);    //100
-        Console.WriteLine(c2.x);    //200
+        Console.WriteLine("\nTestCars2");
+        Console.WriteLine("-----------");
+                                               //Car의 파생클래스이므로 가능
+        var cars = new List<Car> { new Car(), new ConvertibleCar(), new Minivan() };
+        foreach (var car in cars)
+        {
+            car.DescribeCar();
+            Console.WriteLine("-----------");
+        }
+    }
+    public static void TestCars3()
+    {
+        Console.WriteLine("\nTestCar3");
+        Console.WriteLine("-----------");
+        ConvertibleCar car2 = new ConvertibleCar();
+        Minivan car3 = new Minivan();
+        car2.ShowDetails();
+        car3.ShowDetails();
+    }
+    
+    static void Main()
+    {
+        TestCars1();
+        TestCars2();
+        TestCars3();
     }
 }
+
