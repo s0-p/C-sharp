f3aed4749d50b276f6c6cd0abfcdffbfd2caff57
s0-p <b00v0429@gmail.com>
2023-04-26 18:56:37 +0900

    instanciate parent class before instanciate  child class

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,33 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Character
+class Animal
 {
-    public virtual void ReadyToBattle()
+    public Animal()
     {
-        Console.WriteLine("전투 태세를 취합니다.");
+        Console.WriteLine("나는 동물입니다.");
+    }
+    public virtual void Bark()
+    {
+        Console.WriteLine("울음소리를 냅니다.");
+    }
+    public virtual void Attack()
+    {
+        Console.WriteLine("공격을 시작합니다.");
     }
 }
-public class Knight : Character
+class Cat : Animal
 {
-    public override void ReadyToBattle()
+    public Cat()
     {
-        Console.WriteLine("기사 : 칼을 뽑아 전투 태세를 취합니다.");
+        Console.WriteLine("나는 고양이입니다.");
     }
-    public override string ToString()
+    public override void Bark()
     {
-        return "기사 클래스";
+        Console.WriteLine("냐옹");
+    }
+    public override void Attack()
+    {
+        Console.WriteLine("핡퀴기 공격");
     }
 }
-public class Acher : Character
+class Dog : Animal
 {
-    public override void ReadyToBattle()
+    public Dog()
     {
-        Console.WriteLine("궁수 : 활을 뽑아 전투 태세를 취합니다.");
+        Console.WriteLine("나는 개입니다.");
     }
-    public override string ToString()
+    public override void Bark()
     {
-        return "궁수 클래스";
+        Console.WriteLine("멍멍");
+    }
+    public override void Attack()
+    {
+        Console.WriteLine("물기 공격");
     }
 }
 
@@ -38,16 +54,35 @@ class Program
 {
     static void Main()
     {
-        Character[] characters = new Character[2];
-
-        characters[0] = new Knight();
-        characters[1] = new Acher();
-
-        for (int i = 0; i < characters.Length; i++)
+        Animal ani = new Animal();
+        Cat cat = new Cat();    //Cat을 instanciate 하기 전에 Animal을 instanciate
+        Dog dog = new Dog();
+        ani.Bark();
+        cat.Bark();
+        dog.Bark();
+        List<Animal> listAni = new List<Animal>() { new Animal(), new Cat(), new Dog() };
+        for (int i = 0; i < listAni.Count; i++)
         {
-            Console.WriteLine(characters[i].ToString());
-            characters[i].ReadyToBattle();
+            listAni[i].Attack();
         }
     }
 }
+
+//결과
+//나는 동물입니다.
+//나는 동물입니다.
+//나는 고양이입니다.
+//나는 동물입니다.
+//나는 개입니다.
+//울음소리를 냅니다.
+//냐옹
+//멍멍
+//나는 동물입니다.
+//나는 동물입니다.
+//나는 고양이입니다.
+//나는 동물입니다.
+//나는 개입니다.
+//공격을 시작합니다.
+//핡퀴기 공격
+//물기 공격
 
