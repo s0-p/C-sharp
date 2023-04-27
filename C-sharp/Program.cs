d4aa0b0265040b3206fccf6f8af2e9d9f3e21977
s0-p <b00v0429@gmail.com>
2023-04-27 18:15:29 +0900

    abstract class

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+abstract class Abstract
+{
+    protected int x = 10;
+    public abstract int X { get; set; }
+    public abstract void Func();
+}
+class General : Abstract
+{
+    public override int X
+    {
+        get { return x + 20; }
+        set { x = value; }
+    }
+    public override void Func()
+    {
+        Console.WriteLine("추상화 클래스 구현");
+    }
+}
 
 class Program
 {
     static void Main()
     {
-        Stage stage = new Stage();
-        stage.Start();
-        stage.DrawMonster();
+        General gn = new General();
+        Console.WriteLine("프로퍼티 X의 값 : " + gn.X);
+        gn.Func();
     }
 }
