08ebff8c2fed4af2899a2de76466452225cf24a1
s0-p <b00v0429@gmail.com>
2023-04-28 18:16:18 +0900

    keyword : is, as

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,72 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Student
+class BaseC
 {
-    public int num { get; set; }
-    public string name { get; set; }
-}
-public class StudentList
-{
-    public List<Student> list = new List<Student>();
-    public int Count { get { return list.Count; } }
-    public void Add(Student std)
+    public virtual void PrintMethod()
     {
-        list.Add(std);
-    }
-    public void Clear()
-    {
-        list.Clear();
-    }
-    Ascending sortAscending = new Ascending();
-    Descending sortDescending = new Descending();
-    public void SorAscending()
-    {
-        list.Sort(sortAscending);
-    }
-    public void SortDescending()
-    {
-        list.Sort(sortDescending);
-    }
-    class Ascending : IComparer<Student>
-    {
-        public int Compare(Student a, Student b)
-        {
-            return a.num.CompareTo(b.num);
-        }
-    }
-    class Descending : IComparer<Student>
-    {
-        public int Compare(Student a, Student b)
-        {
-            return b.num.CompareTo(a.num);
-        }
+        Console.WriteLine("부모 클래스 Method");
     }
 }
-
+class DrivedC : BaseC
+{
+    public override void PrintMethod()
+    {
+        Console.WriteLine("부모 Method 재정의");
+    }
+    public void PrintMethod2()
+    {
+        Console.WriteLine("자식 클래스 Method");
+    }
+}
 
 class Program
 {
+    void CastingTest(object obj)
+    {
+        BaseC baseC;
+        bool already = obj is BaseC;
+        if (already)
+        {
+            baseC = obj as BaseC;
+            if (baseC != null)
+            {
+                baseC.PrintMethod();
+            }
+        }
+        BaseC baseC2 = (BaseC)obj;
+        baseC2.PrintMethod();
+    }
     static void Main()
     {
-        List<Student> tmplist = new List<Student>() { new Student() { num=1, name="소영"},
-                                                      new Student() { num=2, name="수지"},
-                                                      new Student() { num=3, name="설현"},
-        };
-        StudentList stdList = new StudentList();
-        for (int i = 0; i < tmplist.Count; i++)
-        {
-            stdList.Add(tmplist[i]);
-        }
-        stdList.SortDescending();
-        for (int i = 0; i < stdList.Count; i++)
-        {
-            Console.WriteLine("번호 : {0}, 이름 : {1}", stdList.list[i].num, stdList.list[i].name);
-        }
-        stdList.SorAscending();
-        for (int i = 0; i < stdList.Count; i++)
-        {
-            Console.WriteLine("번호 : {0}, 이름 : {1}", stdList.list[i].num, stdList.list[i].name);
-        }
+        DrivedC drivedC = new DrivedC();
+        new Program().CastingTest(drivedC);
+        drivedC.PrintMethod();
+        drivedC.PrintMethod2();
     }
 }
