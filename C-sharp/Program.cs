6c216e7b304c5a17e33056ed7fce41bfc4806b21
s0-p <b00v0429@gmail.com>
2023-04-28 18:57:24 +0900

    write to file

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 using System.IO;
 
 class Program
-{   private static void FileRead(string filename)
+{   private static void FileWrite(string fileName, string text)
     {
-        string path = @"D:\test\" + filename;
-        FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
-        StreamReader sr = new StreamReader(file);
-        string readText = sr.ReadToEnd();
-        sr.Close();
-        file.Close();
-        Console.WriteLine(readText);
+        string path = @"D:\test\" + fileName;
+        FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine(text);
+        //sw.Flush();
+        sw.Close();
+        Console.WriteLine("저장 완료");
     }
     static void Main()
     {
         Console.WriteLine("읽어올 파일 이름을 입력해 주세요.");
-        string filename = Console.ReadLine();
-        FileRead(filename);
+        string fileName = Console.ReadLine();
+        FileWrite(fileName, "안녕하세요 SBS 게임 아카데미 입니다.");
     }
 }
