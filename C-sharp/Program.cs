423cf020571a5a83ac7ac2c87006e2581c817000
s0-p <b00v0429@gmail.com>
2023-04-28 19:32:07 +0900

    binary file IO

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -7,29 +7,30 @@ using System.IO;
 
 class Program
 {   
-    public static byte[] WriteStringBytes(string str, FileStream fs)
-    {
-        byte[] byteStr = new UTF8Encoding(true).GetBytes(str);
-        fs.Write(byteStr, 0, byteStr.Length);
-        return byteStr;
-    }
-    private static void FileRead(string fileName)
+    private static void FileCreate(string fileName)
     {
         string path = @"D:\test\" + fileName;
-        string outStr = "안녕하세요. 박소영입니다.";
-        byte[] readByte = new byte[1024];
-        FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-        WriteStringBytes(outStr, file);
-        file.Seek(0, SeekOrigin.Begin);
-        UTF8Encoding utf8 = new UTF8Encoding(true);
-        file.Read(readByte, 0, readByte.Length);
-        Console.WriteLine(utf8.GetString(readByte));
-        file.Close();
+        FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+        BinaryWriter bw = new BinaryWriter(fs);
+        bw.Write(int.MaxValue);
+        bw.Write(double.MaxValue);
+        bw.Write("안녕하세요. 박소영입니다.");
+        fs.Close();
+
+        fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        BinaryReader br = new BinaryReader(fs);
+        Console.WriteLine("File size : {0} bytes", br.BaseStream.Length);
+        Console.WriteLine(br.ReadInt32());
+        Console.WriteLine(br.ReadDouble());
+        Console.WriteLine(br.ReadString());
+        fs.Close();
+
     }
     static void Main()
     {
-        Console.Write("파일 이름을 입력해주세요 : ");
+        Console.Write("생성할 파일 이름을 입력해주세요 : ");
         string fileName = Console.ReadLine();
-        FileRead(fileName);
+        FileCreate(fileName);
     }
+    
 }
