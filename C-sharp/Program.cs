fae84c12da4b03bdfdf7b9f1b40d82bc9cf34f2f
s0-p <b00v0429@gmail.com>
2023-04-28 18:46:11 +0900

    File IO

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -3,42 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-interface Polygon
-{
-    int area(int a);
-
-    int perimeter(int a);
-}
-class Squre : Polygon
-{
-    public int area(int a)
-    {
-        return a * a;
-    }
-    public int perimeter(int a)
-    {
-        return 4 * a;
-    }
-}
-class Triangle : Polygon
-{
-    public int area(int a)
-    {
-        return a * a / 2;
-    }
-    public int perimeter(int a)
-    {
-        return 3 * a;
-    }
-}
+using System.IO;
 
 class Program
-{
+{   private static void FileRead(string filename)
+    {
+        string path = @"C:\test\" + filename;
+        TextReader tr = new StreamReader(path);
+        string readText = tr.ReadToEnd();
+        tr.Close();
+        Console.WriteLine(readText);
+    }
     static void Main()
     {
-        Polygon sq = new Squre();
-        Polygon tri = new Triangle();
-        Console.WriteLine("area: {0}, perimeter: {1}", sq.area(2), sq.perimeter(4));
-        Console.WriteLine("area: {0}, perimeter: {1}", tri.area(3), tri.perimeter(5));
+        Console.WriteLine("읽어올 파일 이름을 입력해 주세요.");
+        string filename = Console.ReadLine();
+        FileRead(filename);
     }
 }
