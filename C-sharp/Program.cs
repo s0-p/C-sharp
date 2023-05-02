d7d5de4c5b1be8a45375391b30fb94e9bb2c4e0a
s0-p <b00v0429@gmail.com>
2023-05-02 19:41:52 +0900

    anonymous type

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -7,41 +7,24 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 class Program
-{
-    static bool checkFiles(string s)
-    {
-        string[] exts = new[] { ".bmp", ".txt", ".gif" };
-        return exts.Contains(Path.GetExtension(s), StringComparer.OrdinalIgnoreCase);   // 확장자, 대소문자 구분x
-    }
-    static void GetFiles()
-    {
-        string path = @"D:\test\TestDirectory";
-        try
-        {
-            DirectoryInfo dinfo = new DirectoryInfo(path);
-            if (dinfo.Exists)
-            {
-                FileInfo[] files = dinfo.GetFiles("*.*", SearchOption.TopDirectoryOnly);
-                string[] fileStr = new string[files.Length];
-                for (int i = 0; i < files.Length; i++)
-                {
-                    fileStr[i] = files[i].ToString();
-                }
-                string[] result = fileStr.Where(checkFiles).ToArray();
-                for (int i = 0; i < result.Length; i++)
-                {
-                    Console.WriteLine(result[i]);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-    }
+{ 
     static void Main()
     {
-        GetFiles();
+        var temp = new { age = 11, name = "민수" };
+        Console.WriteLine("이름 : {0}, 나이 : {1}", temp.name, temp.age);
+        var tempArray = new
+        {
+            Int = new int[] { 100, 200, 300, 400 },
+            Float = new float[] { 0.5f, 1.5f, 2.5f }
+        };
+        for (int i = 0; i < tempArray.Int.Length; i++)
+        {
+            Console.WriteLine("tempArray.Int[{0}] : {1}", i, tempArray.Int[i]);
+        }
+        for (int i = 0; i < tempArray.Float.Length; i++)
+        {
+            Console.WriteLine("tempArray.Float[{0}] : {1}", i, tempArray.Float[i]);
+        }
     }
     
 }
