209902e41d8ba4785c5535470b85a50960df82a7
s0-p <b00v0429@gmail.com>
2023-05-02 18:34:50 +0900

    FileInfo

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -6,31 +6,26 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-[Serializable]
-class Student
-{
-    public int num { get; set; }
-    public string name { get; set; }
-}
-
 class Program
 {   
     private static void FileCreate(string fileName)
     {
         string path = @"D:\test\" + fileName;
-        Student me = new Student() { num = 1, name = "소영" };
-        FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
-        BinaryFormatter bf = new BinaryFormatter();
-        bf.Serialize(fs, me);   //직렬화
-        fs.Close();
-        fs = new FileStream(path, FileMode.Open, FileAccess.Read);
-        var result = (Student)bf.Deserialize(fs);  //역직렬화
-        fs.Close();
-        Console.WriteLine("번호 = {0}, 이름 = {1}", result.num, result.name);
+        FileInfo file = new FileInfo(path);
+
+        if (!file.Exists)
+        {
+            var sw = file.CreateText();
+        }
+        else
+        {
+            Console.WriteLine("파일이 이미 있습니다.");
+        }
     }
+    
     static void Main()
     {
-        Console.Write("생성할 파일 이름을 입력해주세요 : ");
+        Console.Write("생성할 파일 이름을 입력해주세요: ");
         string fileName = Console.ReadLine();
         FileCreate(fileName);
     }
