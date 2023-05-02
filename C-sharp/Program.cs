1b437c64f742000020b10246cc39d4192a0bd220
s0-p <b00v0429@gmail.com>
2023-05-02 18:56:17 +0900

    save file

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -8,20 +8,48 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 class Program
 {
-    private static void FileRename ()
+    private static void FileSave(string fileName, string text)
     {
-        FileInfo file = new FileInfo(@"D:\test\test.txt");
-
+        string path = string.Format(@"D:\test\{0}", fileName);
+        FileInfo file = new FileInfo(path);
         if (file.Exists)
         {
-            file.MoveTo(@"D:\test\temp.txt");
+            Console.WriteLine("같은 파일이 존재합니다. 덮어 씌우시겠습까? (yes, no) ");
+            string result = Console.ReadLine();
+            if (result == "yes")
+            {
+                StreamWriter writer = file.CreateText();
+                writer.WriteLine(text);
+                writer.Close();
+            }
+            else
+            {
+                int count = 0;
+                FileInfo tmpFile = null;
+                do
+                {
+                    path = string.Format(@"D:\test\사본{0}_{1}", count, fileName);
+                    tmpFile = new FileInfo(path);
+                    count++;
+                } while (tmpFile.Exists);
+                StreamWriter writer = tmpFile.CreateText();
+                writer.WriteLine(text);
+                writer.Close();
+            }
         }
-        
+        else
+        {
+            StreamWriter writer = file.CreateText();
+            writer.WriteLine(text);
+            writer.Close();
+        }
     }
     
     static void Main()
     {
-        FileRename();
+        Console.Write("파일 이름을 입력해주세요: ");
+        string path = Console.ReadLine();
+        FileSave(path, "hello world");
     }
     
 }
