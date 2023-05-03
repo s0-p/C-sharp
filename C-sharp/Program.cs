4096732acf9ee3325e197038bf915f62d9c43455
s0-p <b00v0429@gmail.com>
2023-05-03 19:16:23 +0900

    singleton (design pattern)

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,20 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+
+class Singleton
+{
+    #region Constants and Fields
+    private static Singleton _instance = null;
+    #endregion
+    
+    #region Public Properties
+    private Singleton() { }
+    public static Singleton Instance
+    {
+        get {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    #region Public Methods and Operators
+    public void DrawMessage()
+    {
+        Console.WriteLine("안녕하세요. SBS 게임 아카데미 입니다.");
+    }
+    #endregion
+}
+
 class Program
 {
+
     static void Main()
     {
-        Action<string> Print = (str) => Console.WriteLine(str);
-        Action<int, int> Add = (a, b) => Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
-        Action<int, int> Mul = (a, b) => Console.WriteLine("{0} + {1} = {2}", a, b, a * b);
-        Action<double> FtoC = (F) => Console.WriteLine("화씨 {0} -> 섭씨 {1}", F, (F - 32) * 5 / 9);
-        Action<double> CtoF = (C) => Console.WriteLine("섭씨 {0} -> 화씨 {1}", C, C * 9 / 5 + 32);
-
-        Print("안녕하세요. SBS 게임 아카데미입니다.");
-        Add(10, 20);
-        FtoC(132.2d);
-        CtoF(36.5d);
+        Singleton.Instance.DrawMessage();
     }
 
 }
+
+//#region Constants and Fields
+//#endregion
+//#region Public Properties
+//#endregion
+//#region Public Methods and Operators
+//#endregion
