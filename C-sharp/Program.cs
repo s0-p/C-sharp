4eb408e0a886f4a423f8ae6ebd81a91b590a7432
s0-p <b00v0429@gmail.com>
2023-05-03 18:49:05 +0900

    Action (delegate)

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,22 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-
 class Program
 {
     static void Main()
     {
-        Func<string> Print = () => "안녕하세요. SBS 게임 아카데미입니다.";
-        Func<int, int, int> Add = (a, b) => a + b;
-        Func<int, int, int> Mul = (a, b) => a * b;
-        Func<double, double> FtoC = (F) => (F - 32) * 5 / 9;
-        Func<double, double> CtoF = (C) => C * 9 / 5 + 32;
+        Action<string> Print = (str) => Console.WriteLine(str);
+        Action<int, int> Add = (a, b) => Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+        Action<int, int> Mul = (a, b) => Console.WriteLine("{0} + {1} = {2}", a, b, a * b);
+        Action<double> FtoC = (F) => Console.WriteLine("화씨 {0} -> 섭씨 {1}", F, (F - 32) * 5 / 9);
+        Action<double> CtoF = (C) => Console.WriteLine("섭씨 {0} -> 화씨 {1}", C, C * 9 / 5 + 32);
 
-        Console.WriteLine("Func Print 값 : {0}", Print());
-        Console.WriteLine("Func Add 값 : {0}", Add(10, 20));
-        Console.WriteLine("Func FtoC 값 : {0}", FtoC(132.2d));
-        Console.WriteLine("Func CtoF 값 : {0}", CtoF(36.5d));
+        Print("안녕하세요. SBS 게임 아카데미입니다.");
+        Add(10, 20);
+        FtoC(132.2d);
+        CtoF(36.5d);
     }
-    
+
 }
