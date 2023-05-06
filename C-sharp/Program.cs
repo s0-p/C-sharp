944740494a50056c51eaa46ae401cc3895096f8f
s0-p <b00v0429@gmail.com>
2023-05-06 16:47:55 +0900

    StringBuilder

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,139 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
-public class Monster
-{
-    #region Constants and Fields
-    public int _hp { get; set; }
-    public int _def { get; set; }
-    public int _atk { get; set; }
-    public bool _isAlive { get; set; }
-    #endregion
-    #region Public Properties
-    public void InitMonster()
-    {
-        _hp = 100;
-        _def = 20;
-        _atk = 150;
-        _isAlive = true;
-    }
-    #endregion
-    #region Public Methods and Operators
-    public void MonsterInfo()
-    {
-        Console.WriteLine("HP : {0}", _hp);
-        Console.WriteLine("DEF : {0}", _def);
-        Console.WriteLine("ATK : {0}", _atk);
-        Console.WriteLine();
-    }
-    #endregion
-}
-public class Singleton<T> where T : class, new()
-{   
-    static Singleton()
-    {
-        if (Singleton<T>.Instance == null)
-        {
-            Singleton<T>.Instance = new T();
-        }
-    }
-    public static T Instance { get; private set; }
-
-    
-}
-
-public class MonsterManager : Singleton<MonsterManager>
-{
-    #region Constants and Fields
-    List<Monster> _monList = new List<Monster>();
-    #endregion
-    #region Public Properties
-    public int Count {  get { return _monList.Count; } }
-    public Monster GetMonster(int idx)
-    {
-        return _monList[idx];
-    }
-    public void InitMonsters()
-    {
-        for (int i = 0; i < _monList.Count; i++)
-        {
-            _monList[i].InitMonster();
-        }
-    }
-    #endregion
-    #region Public Methods and Operators
-    public void Add(Monster mon)
-    {
-        _monList.Add(mon);
-    }
-    public bool Remove(Monster mon)
-    {
-        return _monList.Remove(mon);
-    }
-    public void RemoveAll()
-    {
-        _monList.RemoveAll(element => element._isAlive == true);
-    }
-    public void Clear()
-    {
-        _monList.Clear();
-    }
-    
-    public void DrawonstersInfo()
-    {
-        for (int i = 0; i < _monList.Count; i++)
-        {
-            _monList[i].MonsterInfo();
-
-        }
-    }
-    #endregion
-}
-public class Stage
-{
-    #region Constants and Fields
-    const int MONSTER_MAX = 10;
-    #endregion
-    #region Public Properties
-
-    #endregion
-    #region Public Methods and Operators
-    public void CreateMonsters()
-    {
-        for (int i = 0; i < MONSTER_MAX; i++)
-        {
-            MonsterManager.Instance.Add(new Monster());
-        }
-        MonsterManager.Instance.InitMonsters();
-    }
-    public bool IsClear()
-    {
-        for (int i = 0; i < MonsterManager.Instance.Count; i++)
-        {
-            Monster mon = null;
-            if ((mon = MonsterManager.Instance.GetMonster(i)) != null)
-            {
-                if (mon._isAlive)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    #endregion
-}
+using System.Text;
 
 class Program
 {
 
     static void Main()
     {
-        Stage stage = new Stage();
-        stage.CreateMonsters();
-        MonsterManager.Instance.DrawonstersInfo();
-        Console.WriteLine("Stage Clear : {0}", stage.IsClear());
+        StringBuilder sb = new StringBuilder("안녕하세요!");
+        Console.WriteLine(sb);
+        sb.Append(" SBS 게임 아카데미입니다.");
+        Console.WriteLine("Append() : {0}", sb);
+        sb.AppendFormat(" 제 이름은 {0}이고 나이는 {1}살입니다.", "박소영", 24);
+        Console.WriteLine("AppendFormat() : {0}", sb);
+        sb.Remove(0, 6);
+        Console.WriteLine("Remove() : {0}", sb);
+        sb.Insert(12, "강남지점");
+        Console.WriteLine("Insert() : {0}", sb);
+        sb.Replace("강남지점", "신촌지점");
+        Console.WriteLine("Replace() : {0}", sb);
+        sb.Clear();
+        Console.WriteLine("Clear() : {0}", sb);
     }
 
 }
