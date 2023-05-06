3fa4a01e577d0adfa8e3eac7980ad7e34b3b1340
s0-p <b00v0429@gmail.com>
2023-05-06 20:38:45 +0900

    DataSet and DataTable

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -4,27 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Text;
-
+using System.Data;
 class Program
 {
+    const string TableName = "클래스";
+    static DataSet CreateDataSet()
+    {
+        DataSet ds = new DataSet();
+        ds.Tables.Add(TableName);
+        ds.Tables[TableName].Columns.Add("이름");
+        ds.Tables[TableName].Columns.Add("속성");
+        ds.Tables[TableName].Columns.Add("공격력");
+        string[] str = new string[3];
+        str[0] = "나이트";
+        str[1] = "근거리";
+        str[2] = "150";
+        ds.Tables[TableName].Rows.Add(str);
+        str[0] = "레인저";
+        str[1] = "원거리";
+        str[2] = "200";
+        ds.Tables[TableName].Rows.Add(str);
+        str[0] = "소서러";
+        str[1] = "원거리";
+        str[2] = "300";
+        ds.Tables[TableName].Rows.Add(str);
+        return ds;
+    }
 
     static void Main()
     {
-        StringBuilder sb = new StringBuilder("안녕하세요!");
-        Console.WriteLine(sb);
-        sb.Append(" SBS 게임 아카데미입니다.");
-        Console.WriteLine("Append() : {0}", sb);
-        sb.AppendFormat(" 제 이름은 {0}이고 나이는 {1}살입니다.", "박소영", 24);
-        Console.WriteLine("AppendFormat() : {0}", sb);
-        sb.Remove(0, 6);
-        Console.WriteLine("Remove() : {0}", sb);
-        sb.Insert(12, "강남지점");
-        Console.WriteLine("Insert() : {0}", sb);
-        sb.Replace("강남지점", "신촌지점");
-        Console.WriteLine("Replace() : {0}", sb);
-        sb.Clear();
-        Console.WriteLine("Clear() : {0}", sb);
+        DataSet ds = CreateDataSet();
+        DataTable dt = ds.Tables[TableName];
+        DataRow[] dr = dt.Select();
+        for (int i = 0; i < dt.Columns.Count; i++)
+        {
+            Console.Write(dt.Columns[i].ToString() + "\t");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < dr.Length; i++)
+        {
+            for (int j = 0; j < dt.Columns.Count; j++)
+            {
+                Console.Write(dr[i][j] + "\t");
+            }
+            Console.WriteLine();
+        }
     }
 
 }
